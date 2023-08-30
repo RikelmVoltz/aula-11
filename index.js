6325214a640da78f5e53dfef19a9016e22856c38
RikelmVoltz <142436269+RikelmVoltz@users.noreply.github.com>
2023-08-30 17:22:10 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+// function imprimirOlaMundp() {
+//  console.log("olá mundo!")
+// }
+// imprimirOlaMundo();
+ 
+// function escreverNome(nome){
+  // console.log(nome);
+// }
+// escreverNome("rikelme") 
+/*
+const a = 1
+function imprimirVariavel () {       
+    const b = 2
+    console.log('variavel a', a)
+    console.log('variavel b', b)
+
+}
+
+imprimirVariavel()
+console.log('variavel a', a)
+console.log('variavel b', b)
+
+function calculaArea(altura,largura) {
+       const area = altura * largura
+       return area
+
+      }
+
+      const areaCalculada = calculaArea(5,8);
+      console.log("variavel areacalculada:" + areaCalculada);
+      console.log("chamada da funçao crua:" + calculaArea(5,3));
+
+
+
+      function numeros(numeroUM,numeroDois) {
+ const soma = numeroUM + numeroDois
+ return soma
+      }
+
+let somas = numeros(1,2);
+console.log("soma:" + somas);  
+
+ 
+function divideArray(meuArrey){
+  let b = [(meuArrey[0] /2), meuArrey[meuArrey.length-1] /2];
+  return b;
+}
+let a = [1,2,3,4,5,6,7,8,9,10,11,12]; 
+const resultado = divideArray(a) 
+console.log(resultado);
+*/
